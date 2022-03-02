90a12851b49206656384874337902a11723fe827
Ploofafa <mixovith@mail.ru>
2022-03-02 12:00:41 +0700

    1

--- Programming/Programming/View/MainForm.cs
+++ Programming/Programming/View/MainForm.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Programming.Model;
 using Programming.Model.Enums;
 
 namespace Programming.View
