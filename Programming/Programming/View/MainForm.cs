d4b4b1f05880a070fa018a2d003e171dbceded63
Ploofafa <mixovith@mail.ru>
2022-03-02 11:20:36 +0700

    1

--- Programming/Programming/View/MainForm.cs
+++ Programming/Programming/View/MainForm.cs
@@ -22,6 +22,7 @@ namespace Programming.View
             SeasonComboBox.DataSource = Enum.GetValues(typeof(Season));
             EnumListBox.SelectedIndex = 0;
         }
+
         private void EnumListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             IndexBox.Clear();
@@ -30,18 +31,23 @@ namespace Programming.View
                 case Programming.View.Enums.Colors:
                     ValuesListBox.DataSource = Enum.GetValues(typeof(Weekday));
                     break;
+
                 case Programming.View.Enums.Genre:
                     ValuesListBox.DataSource = Enum.GetValues(typeof(Genre));
                     break;
+
                 case Programming.View.Enums.Weekday:
                     ValuesListBox.DataSource = Enum.GetValues(typeof(Colors));
                     break;
+
                 case Programming.View.Enums.StudyForm:
                     ValuesListBox.DataSource = Enum.GetValues(typeof(StudyForm));
                     break;
+
                 case Programming.View.Enums.Manufacturers:
                     ValuesListBox.DataSource = Enum.GetValues(typeof(Manufacturers));
                     break;
+
                 case Programming.View.Enums.Season:
                     ValuesListBox.DataSource = Enum.GetValues(typeof(Season));
                     break;
@@ -54,12 +60,12 @@ namespace Programming.View
             IndexBox.Text = ((int)curItem).ToString();
         }
 
-
         private void SeasonButton_Click(object sender, EventArgs e)
         {
             WinterLabel.Visible = false;
             GroupBoxSeason.BackColor = Color.White;
             SeasonComboBox.ForeColor = Color.Black;
+            
             switch (SeasonComboBox.SelectedItem)
             { 
                 case Season.Summer:
@@ -87,6 +93,7 @@ namespace Programming.View
             {
                 ParseTextOutput.Text = $"Это день недели ({day.ToString()} = {(int)day + 1}).";
             }
+            
             else
             {
                 ParseTextOutput.Text = "Такого дня недели не существует :(";
