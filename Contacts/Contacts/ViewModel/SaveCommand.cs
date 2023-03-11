e78e33ffe5c42f22908110a9be2aa7ce30ab7520
Ploofafa <mixovith@mail.ru>
2023-03-11 15:34:35 +0700

    7 fix(MainWindox.Xaml): поправлена вёрстка приложения

---
+++ Contacts/Contacts/ViewModel/SaveCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contacts.Model.Services;
+using System.Windows.Input;
+
+namespace Contacts.Model
+{
+    class SaveCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<bool> _canExecute;
+
+        event EventHandler? ICommand.CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            this._execute(ContactSerializer.SaveContact);
+        }
+    }
+}
