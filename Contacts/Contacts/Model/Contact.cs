305f34649ff0551b72c1cbd0a427aabfc02ffa9b
Ploofafa <mixovith@mail.ru>
2023-03-11 12:58:12 +0700

    5 View(MainWindow.xaml): сделана вёрстка главного окна

---
+++ Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Model
+{
+    /// <summary>
+    /// Класс описывающий контакт записной книги.
+    /// </summary>
+    internal class Contact
+    {
+        /// <summary>
+        /// Поле, хранящее имя контакта.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Поле, хранящее номер телефона.
+        /// </summary>
+        private string _phoneNumber;
+
+        /// <summary>
+        /// Поле, хранящее почту контакта.
+        /// </summary>
+        private string _email;
+        
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Contact()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор для создания экземпляра класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Переменная, хранящая имя контакта.</param>
+        /// <param name="phoneNumber">Переменная, хранящая номер контакта.</param>
+        /// <param name="email">Переменная, хранящая почту контакта.</param>
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Задаёт и возвращает имя контакта.
+        /// </summary>
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return PhoneNumber;
+            }
+
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+            }
+        }
+    }
+}
