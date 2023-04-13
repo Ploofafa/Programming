07b0163beaf95e6f4298f2b032e3be83b5f65bf2
Ploofafa <loltotkin@gmail.com>
2023-04-13 17:42:32 +0700

    final (fix): теперь не принятые изменения не сохраняются, переделана вёрстка

---
+++ src/Contacts/Contacts/View/BooleanToVisibilityConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Contacts.Model.Services
+{
+    /// <summary>
+    /// Класс реализующий преобразование типа <see cref="bool"/> к <see cref="Visibility"/>.
+    /// </summary>
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Метод преобразующий bool к Visibility.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = false;
+            if (value is bool)
+            {
+                bValue = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                bool? tmp = (bool?)value;
+                bValue = tmp.HasValue ? tmp.Value : false;
+            }
+            return bValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Метод который конвертирует из <see cref="Visibility"/> в <see cref="bool"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- src/Contacts/Contacts/ViewModel/MainVM.cs
+++ src/Contacts/Contacts/ViewModel/MainVM.cs
@@ -1,12 +1,18 @@
 ﻿    using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Media.Media3D;
 using Contacts.Model;
 using Contacts.Model.Services;
 
@@ -18,141 +24,184 @@ namespace Contacts.ViewModel
     class MainVM : INotifyPropertyChanged
     {
         /// <summary>
-        /// Событие для отслеживания изменений в GUI.
+        /// Хранит выбранный экземпляр класса <see cref="Contact"/> в 
+        /// <see cref="ObservableCollection{T}"/> для связи с View.
         /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
+        private Contact _selectedContact;
 
         /// <summary>
-        /// Переменная, хранящая экземпляр класса <see cref="Contact"/>
-        /// для отслеживания состояния объекта в реальном времени.
+        /// Хранит экземпляр класса <see cref="RelayCommand"/>.
+        /// Реализует команду добавления контакта в коллекцию.
         /// </summary>
-        private Contact _contact = new Contact();
+        private RelayCommand _addCommand;
 
         /// <summary>
-        /// Переменная, хранящая делегат для сохранения.
+        /// Хранит экземпляр класса <see cref="RelayCommand"/>.
+        /// Реализует команду удаления контакта из коллекции.
         /// </summary>
-        private RelayCommand _saveCommand;
-    
-        /// <summary>
-        /// Переменная, хранящая делегат для загрузки.
-        /// </summary>
-        private RelayCommand _loadCommand;
+        private RelayCommand _removeCommand;
 
         /// <summary>
-        /// Конструктор класса <see cref="MainVM"/>.
+        /// Хранит экземпляр класса <see cref="RelayCommand"/>.
+        /// Реализует команду редактирования контакта в коллекции.
         /// </summary>
+        private RelayCommand _editCommand;
+
+        /// <summary>
+        /// Хранит экземпляр класса <see cref="RelayCommand"/>.
+        /// Реализует команду для применения изменений контакта в коллекции.
+        /// </summary>
+        private RelayCommand _applyCommand;
+
+        private Contact CloneContact { get; set; }
+
+        private bool _isReadOnly = true;
+
+        public ObservableCollection<Contact> Contacts{ get; set; } = new ObservableCollection<Contact>();
+
+        public bool IsVisible
+        {
+            get
+            {
+                return !_isReadOnly;
+            }
+
+            private set
+            {
+                _isReadOnly = !value;
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return _isReadOnly;
+            }
+
+            private set
+            {
+                _isReadOnly = value;
+                OnPropertyChanged(nameof(IsReadOnly));
+            }
+        }
+
+        public Contact SelectedContact
+        {
+            get
+            {
+                return _selectedContact;
+            }
+            set
+            {
+                if (IsVisible == true && CloneContact != null)
+                {
+                    _selectedContact.Name = CloneContact.Name;
+                    _selectedContact.Email = CloneContact.Email;
+                    _selectedContact.PhoneNumber = CloneContact.PhoneNumber;
+                    CloneContact = null;
+                    IsVisible = false;
+                    IsReadOnly = true;
+                }
+                else if (Contacts.IndexOf(_selectedContact) == -1)
+                {
+                    IsVisible = false;
+                    IsReadOnly = true;
+                }
+                _selectedContact = value;
+                OnPropertyChanged(nameof(SelectedContact));
+            }
+        }
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return _addCommand ?? new RelayCommand(obj =>
+                {
+                    IsVisible = true;
+                    IsReadOnly = false;
+                    SelectedContact  = new Contact();
+                });
+            }
+        }
+
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return _removeCommand ?? new RelayCommand(obj => 
+                {
+                    int index = Contacts.IndexOf(SelectedContact);
+                    Contacts.Remove(SelectedContact);
+                    if (Contacts.Count > 0 && Contacts.Count - 1 >= index)
+                    {
+                        SelectedContact = Contacts[index];
+                    }
+                    else if (Contacts.Count >= 1)
+                    {
+                        SelectedContact = Contacts[index - 1];
+                    }
+                    else
+                    {
+                        SelectedContact = null;
+                    }
+                }, 
+                    (obj) => Contacts.Count > 0 && SelectedContact != null);
+            }
+        }
+
+        public RelayCommand EditCommand
+        {
+            get
+            {
+                return _editCommand ?? new RelayCommand(obj =>
+                {
+                    IsVisible = true;
+                    IsReadOnly = false;
+                    CloneContact = (Contact)SelectedContact.Clone();
+                },
+                (obj) => Contacts.Count > 0 && SelectedContact != null);
+            }
+        }
+
+        public RelayCommand ApplyCommand
+        {
+            get
+            {
+                return _applyCommand ?? new RelayCommand(obj =>
+                {
+                    IsVisible = false;
+                    IsReadOnly = true;
+                    if (Contacts.IndexOf(SelectedContact) == -1)
+                    {
+                        Contacts.Add(SelectedContact);
+                    }
+                });
+            }
+        }
+
+        public void SaveContacts()
+        {
+            ContactSerializer.SaveContacts(Contacts);
+        }
+
+        public void LoadContacts()
+        {
+            Contacts = ContactSerializer.LoadContact();
+        }
+
         public MainVM()
         {
         }
 
-        /// <summary>
-        /// Задаёт и возвращает имя контакта.
-        /// </summary>
-        public string Name
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
         {
-            get
-            {
-                return _contact.Name;
-            }
-
-            set
-            {
-                if(_contact.Name != value)
-                {
-                    _contact.Name = value;
-                    OnPropertyChanged(nameof(Name));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Задаёт и возвращает почту контакта.
-        /// </summary>
-        public string Email
-        {
-            get
-            {
-                return _contact.Email;
-            }
-
-            set
-            {
-                if (_contact.Email != value)
-                {
-                    _contact.Email = value;
-                    OnPropertyChanged(nameof(Email));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Задаёт и возвращает номер телефона контакта.
-        /// </summary>
-        public string PhoneNumber
-        {
-            get
-            {
-                return _contact.PhoneNumber;
-            }
-            set
-            {
-                if (_contact.PhoneNumber != value)
-                {
-                    _contact.PhoneNumber = value;
-                    OnPropertyChanged(nameof(PhoneNumber));
-                }
-                
-            }
-        }
-
-        /// <summary>
-        /// Возвращает команду для сохранения контакта.
-        /// </summary>
-        public RelayCommand SaveCommand
-        {
-            get
-            {
-                return _saveCommand ?? 
-                    (_saveCommand = new RelayCommand(obj =>
-                    {
-                        ContactSerializer.SaveContact(_contact);
-                    },
-                    obj =>
-                    {
-                        return true;
-                    }));
-            }
-        }
-
-        /// <summary>
-        /// Возвращает метод для загрузки контакта.
-        /// </summary>
-        public RelayCommand LoadCommand
-        {
-            get
-            {
-                return _loadCommand ?? 
-                    (_loadCommand = new RelayCommand(obj =>
-                    {
-                        var contact = ContactSerializer.LoadContact(_contact);
-                        Name = contact.Name;
-                        Email = contact.Email;
-                        PhoneNumber = contact.PhoneNumber;
-                    },
-                    obj =>
-                    {
-                        return true;
-                    }));
-            }
-        }
-
-        /// <summary>
-        /// Метод для отслеживания изменений свойств.
-        /// </summary>
-        /// <param name="propertyName">Имя изменяемого свойства.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
         }
     }
 }
